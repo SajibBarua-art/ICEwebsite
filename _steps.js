3a96559c11943c7828800258de55b5ba4979139b
SajibBarua-art <sajibbaruanstu@gmail.com>
2023-10-21 16:59:05 +0600

    v 0.2

--- _steps.js
+++
@@ -1,5 +0,0 @@
-/* 
-1. Create a post api on the server side
-2. Client site and server data via post
-
-*/
--- index.js
+++ index.js
@@ -1,82 +1,343 @@
+// For backend and express
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+require('dotenv').config();
 app.use(express.json());
 app.use(cors());
-
-const teachers = [];
-const students = [];
-const timeSlot = [
-  {
-    start: '9:00',
-    end: '9:45',
-  },
-  {
-    start: '9:50',
-    end: '10:35',
-  },
-  {
-    start: '10:40',
-    end: '11:25',
-  },
-  {
-    start: '11:30',
-    end: '12:15',
-  },
-  {
-    start: '12:15',
-    end: '1:00',
-  },
-  {
-    start: '2:00',
-    end: '2:50',
-  },
-  {
-    start: '2:55',
-    end: '3:45',
-  },
-];
-
-app.get('/', (req, resp) => {
-  resp.send('App is Working');
+app.get("/", (req, resp) => {
+    resp.send("App is Working");
 });
 
-// Teachers
-app.post('/teachers', (req, resp) => {
-  const teacher = req.body;
-  if (teachers.some((t) => t.email === teacher.email)) {
-    resp.status(400).send('Teacher with the same email already exists');
-  } else {
-    teachers.push(teacher);
-    resp.status(201).json(teacher);
-  }
+// Connect to the database
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.smd1bmx.mongodb.net/?retryWrites=true&w=majority`, {
+    dbName: 'routine',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Connected to routine database');
+})
+.catch(err => {
+    console.error('Error connecting to the database:', err);
 });
 
-app.get('/teachers', (req, resp) => {
-  resp.json(teachers);
+// Schema for teachers of app
+const teacherSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    teacherCode: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+const Teacher = mongoose.model('teachers', teacherSchema);
+ 
+app.post("/teachers", async (req, resp) => {
+    try {
+        const teacher = new Teacher(req.body);
+        let result = await teacher.save();
+        result = result.toObject();
+        if (result) {
+            delete result.password;
+            resp.send(req.body);
+            console.log(result);
+        } else {
+            console.log("teacher already registered");
+        }
+ 
+    } catch (e) {
+        resp.status(400).send("!!! Error in teacher register panel !!!\n" + e);
+    }
 });
 
-// Students
-app.post('/students', (req, resp) => {
-  const student = req.body;
-  if (students.some((s) => s.email === student.email)) {
-    resp.status(400).send('Student with the same email already exists');
-  } else {
-    students.push(student);
-    resp.status(201).json(student);
-  }
+app.get("/teachers", async (req, resp) => {
+    try {
+        // Retrieve all teachers from the MongoDB database
+        const users = await Teacher.find({});
+        resp.json(users); // Send the users as a JSON response
+    } catch (error) {
+        console.error("An error occurred in teachers get function:", error);
+        resp.status(500).send("Internal Server Error");
+    }
 });
 
-app.get('/students', (req, resp) => {
-  resp.json(students);
+// course details
+const courseDetailsSchema = new mongoose.Schema({
+    code: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    term: {
+        type: Number,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+const CourseDetails = mongoose.model('courseDetails', courseDetailsSchema);
+ 
+app.post("/courseDetails", async (req, resp) => {
+    try {
+        const courseDetails = new CourseDetails(req.body);
+        let result = await courseDetails.save();
+        result = result.toObject();
+        if (result) {
+            resp.send(req.body);
+            console.log(result);
+        } else {
+            console.log("courseDetails already registered");
+        }
+ 
+    } catch (e) {
+        resp.status(400).send("!!! Error in courseDetails register panel !!!\n" + e);
+    }
 });
 
-// Time Slot
-app.get('/timeSlot', (req, resp) => {
-  resp.json(timeSlot);
+app.get("/courseDetails", async (req, resp) => {
+    try {
+        // Retrieve all CourseDetails from the MongoDB database
+        const users = await CourseDetails.find({});
+        resp.json(users); // Send the users as a JSON response
+    } catch (error) {
+        console.error("An error occurred in CourseDetails get function:", error);
+        resp.status(500).send("Internal Server Error");
+    }
 });
 
-const port = 5000;
+// teacher course database
+const teacherCourseSchema = new mongoose.Schema({
+    teacherCode: {
+        type: String,
+        required: true
+    },
+    courseCode: {
+        type: String,
+        required: true
+    }
+});
+const TeacherCourse = mongoose.model('teacherCourse', teacherCourseSchema);
+ 
+app.post("/teacherCourse", async (req, resp) => {
+    try {
+        const teacherCourse = new TeacherCourse(req.body);
+        let result = await teacherCourse.save();
+        result = result.toObject();
+        if (result) {
+            resp.send(req.body);
+            console.log(result);
+        } else {
+            console.log("teacherCourse already registered");
+        }
+ 
+    } catch (e) {
+        resp.status(400).send("!!! Error in teacherCourse register panel !!!\n" + e);
+    }
+});
+
+app.get("/teacherCourse", async (req, resp) => {
+    try {
+        // Retrieve all TeacherCourse from the MongoDB database
+        const users = await TeacherCourse.find({});
+        resp.json(users); // Send the users as a JSON response
+    } catch (error) {
+        console.error("An error occurred in TeacherCourse get function:", error);
+        resp.status(500).send("Internal Server Error");
+    }
+});
+
+// Schema for students of app
+const studentSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    term: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+});
+const Student = mongoose.model('students', studentSchema);
+ 
+app.post("/students", async (req, resp) => {
+    try {
+        const student = new Student(req.body);
+        let result = await student.save();
+        result = result.toObject();
+        if (result) {
+            delete result.password;
+            resp.send(req.body);
+            console.log(result);
+        } else {
+            console.log("student already register");
+        }
+ 
+    } catch (e) {
+        resp.send("!!! Error in student register panel !!!\n", e);
+    }
+});
+
+app.get("/students", async (req, resp) => {
+    try {
+        // Retrieve all students from the MongoDB database
+        const users = await Student.find({});
+        resp.json(users); // Send the users as a JSON response
+    } catch (error) {
+        console.error("An error occurred in students get function:", error);
+        resp.status(500).send("Internal Server Error");
+    }
+});
+
+// create database for time-slot
+const timeSlotSchema = new mongoose.Schema({
+    timeSlot: Array
+});
+
+const TimeSlot = mongoose.model('timeSlot', timeSlotSchema);
+
+const createTimeSlot = async () => {
+    try {
+        const newTimeSlot = new TimeSlot({
+            timeSlot: [
+                {
+                    start: '9:00',
+                    end: '9:45'
+                },
+                {
+                    start: '9:50',
+                    end: '10:35'
+                },
+                {
+                    start: '10:40',
+                    end: '11:25'
+                },
+                {
+                    start: '11:30',
+                    end: '12:15'
+                },
+                {
+                    start: '12:15',
+                    end: '1:00'
+                },
+                {
+                    start: '2:00',
+                    end: '2:50'
+                },
+                {
+                    start: '2:55',
+                    end: '3:45'
+                },
+            ]
+        });
+
+        const timeSlot = await newTimeSlot.save();
+        console.log('Time-slot saved:', timeSlot);
+    } catch (err) {
+        console.error('Error saving time-slot:', err);
+    }
+};
+// createTimeSlot();
+
+// get time-slot
+app.get("/timeSlot", async (req, resp) => {
+    try {
+        const timeSlot = await TimeSlot.find({});
+        resp.json(timeSlot); // Send the users as a JSON response
+    } catch (error) {
+        console.error("An error occurred in time-slot get function:", error);
+        resp.status(500).send("Internal Server Error");
+    }
+});
+
+// create database for room
+const roomSchema = new mongoose.Schema({
+    class: Array,
+    lab: Object
+});
+
+const Room = mongoose.model('room', roomSchema);
+const createRoom = async () => {
+    try {
+        const newRoom = new Room({
+            class: ['809', '810'],
+            lab: {
+                computer: ['808', '812'],
+                electrical: '813'
+            }
+        });
+
+        const room = await newRoom.save();
+        console.log('Room saved:', room);
+    } catch (err) {
+        console.error('Error saving room:', err);
+    }
+};
+// createRoom();
+
+// get room
+app.get("/room", async (req, resp) => {
+    try {
+        const room = await Room.find({});
+        resp.json(room); // Send the users as a JSON response
+    } catch (error) {
+        console.error("An error occurred in room get function:", error);
+        resp.status(500).send("Internal Server Error");
+    }
+});
+
+// create routine randomly
+
+
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
-});
+    console.log(`Example app listening at http://localhost:${port}`)
+})
